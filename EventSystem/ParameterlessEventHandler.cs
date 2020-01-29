5fb0b08e849b8062e5518ad4cecf15d5b07ce94d
Danny van Duist <dvduist@hotmail.nl>
2020-01-29 14:22:41 +0100

    Fixed a typo.

---
+++ EventSystem/ParameterlessEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VDFramework.EventSystem
+{
+	internal class ParameterlessEventHandler : EventHandler
+	{
+		public ParameterlessEventHandler(Delegate callback, int priorityOrder) : base(callback, priorityOrder)
+		{
+		}
+		
+		public void Invoke()
+		{
+			(Callback as Action)?.Invoke();
+		}
+	}
+}
