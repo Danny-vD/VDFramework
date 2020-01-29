09630ef4b731c54e00700ff670df8d29206c5b3b
Danny van Duist <dvduist@hotmail.nl>
2020-01-29 15:26:26 +0100

    Added a start to the Unity framework.

---
+++ VDUnityFramework/BaseClasses/BetterMonoBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace VDUnityFramework.BaseClasses
+{
+	public class BetterMonoBehaviour : MonoBehaviour
+	{
+		private Transform cachedTransform;
+
+		public Transform CachedTransform
+		{
+			get
+			{
+				if (cachedTransform == null)
+				{
+					cachedTransform = transform;
+				}
+
+				return cachedTransform;
+			}
+		}
+	}
+}
