156e763086b1b11e851101ac3047e2f533772375
Danny van Duist <dvduist@hotmail.nl>
2020-02-28 09:28:41 +0100

    fixed the name of singletons

--- VDUnityFramework/Singleton/SingletonInstanceCreator.cs
+++ VDUnityFramework/Singleton/SingletonInstanceCreator.cs
@@ -6,7 +6,7 @@ namespace VDFramework.Singleton
 	{
 		public static T CreateInstance()
 		{
-			GameObject singletonContainer = new GameObject($"{typeof(T)}");
+			GameObject singletonContainer = new GameObject($"{typeof(T)} singleton");
 			return singletonContainer.AddComponent<T>();
 		}
 	}
