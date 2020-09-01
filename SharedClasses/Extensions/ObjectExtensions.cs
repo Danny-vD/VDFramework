7564c1f02a084a6a9be9d9bdea5d2417e533dc0c
Danny van Duist <dvduist@hotmail.nl>
2020-09-01 17:48:40 +0200

    updated the ConvertTo<> extension function.

---
+++ SharedClasses/Extensions/ObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VDFramework.Extensions
+{
+	public static class ObjectExtensions
+	{
+		public static TNewType ConvertTo<TNewType>(this object @object)
+		{
+			try
+			{
+				return (TNewType) Convert.ChangeType(@object, typeof(TNewType));
+			}
+			catch
+			{
+				return (TNewType) @object;
+			}
+		}
+	}
+}
