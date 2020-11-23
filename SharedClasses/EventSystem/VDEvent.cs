5475eb07b8b52f2dce84d31e87d11159451cf4cb
Danny-vD <dvduist@hotmail.nl>
2020-11-23 14:53:40 +0100

    SHARED: + VDEvent<TEvent> class allows you to do: TEvent.Listeners += Action<TEvent> TEvent.ParameterlessListeners += Action
    similar for removing listeners.

--- SharedClasses/EventSystem/VDEvent.cs
+++ SharedClasses/EventSystem/VDEvent.cs
@@ -1,5 +1,22 @@
-﻿namespace VDFramework.EventSystem
+﻿using System;
+
+namespace VDFramework.EventSystem
 {
+	public abstract class VDEvent<TEvent> : VDEvent where TEvent : VDEvent<TEvent>
+	{
+		public static event Action<TEvent> Listeners
+		{
+			add => EventManager.Instance.AddListener(value);
+			remove => EventManager.Instance.RemoveListener(value);
+		}
+		
+		public static event Action ParameterlessListeners
+		{
+			add => EventManager.Instance.AddListener<TEvent>(value);
+			remove => EventManager.Instance.RemoveListener<TEvent>(value);
+		}
+	}
+
 	public abstract class VDEvent
 	{
 		public bool Consumed { get; private set; }
