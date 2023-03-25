c7589bed26be432982ddd7ff2b1cbe496ec2aa2c
Danny-vD <dvduist@hotmail.nl>
2023-03-25 21:53:16 +0100

    Add a DistanceSquaredHSV in ColourExtenions which uses the HSV of the colours instead of the RGB to measure distance

--- VDUnityFramework/UnityExtensions/ColorExtensions.cs
+++ VDUnityFramework/UnityExtensions/ColorExtensions.cs
@@ -12,5 +12,17 @@ namespace VDFramework.UnityExtensions
 
 			return (redDelta * redDelta) + (greenDelta * greenDelta) + (blueDelta * blueDelta);
 		}
+		
+		public static float DistanceSquaredHSV(this Color referenceColour, Color colour)
+		{
+			Color.RGBToHSV(referenceColour, out float hue1, out float saturation1, out float value1);
+			Color.RGBToHSV(colour, out float hue2, out float saturation2, out float value2);
+			
+			float hueDelta = hue2 - hue1;
+			float saturationDelta = saturation2 - saturation1;
+			float valueDelta = value2 - value1;
+
+			return (hueDelta * hueDelta) + (saturationDelta * saturationDelta) + (valueDelta * valueDelta);
+		}
 	}
 }
