460afcc51262ca150a134a03bb2b8db0431bef51
Danny-vD <dvduist@hotmail.nl>
2025-02-18 14:15:36 +0100

    SHARED: Add the golden ratio to the `MathConstants`

--- SharedClasses/Utility/MathUtility/MathConstants.cs
+++ SharedClasses/Utility/MathUtility/MathConstants.cs
@@ -11,5 +11,12 @@ namespace VDFramework.Utility.MathUtility
 		/// The constant 1/3
 		/// </summary>
 		public const double THIRD = 0.3333333333333333D;
+
+		/// <summary>
+		/// The 'extreme and mean ratio' or 'divine proportion' calculated as<br/>(1 + √5) / 2<br/>1.618033988749...
+		/// </summary>
+		/// <symbol>φ (phi)</symbol>
+		/// <theory>https://en.wikipedia.org/wiki/Golden_ratio#Calculation</theory>
+		public static readonly double GoldenRatio = (1 + Math.Sqrt(5)) / 2;
 	}
 }
