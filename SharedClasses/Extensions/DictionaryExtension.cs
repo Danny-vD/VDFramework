b3503cd13ae951bbb6820439b6c63698860d7299
Danny-vD <dvduist@hotmail.nl>
2026-02-04 16:42:51 +0100

    SHARED: Add an extension for Dictionary that automatically adds a new TValue when trying to get the value for a given key

---
+++ SharedClasses/Extensions/DictionaryExtension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VDFramework.Extensions
+{
+	/// <summary>
+	/// Contains Extension methods for Dictionaries
+	/// </summary>
+	public static class DictionaryExtension
+	{
+		/// <summary>
+		/// <para>Get the value of the KeyValuePair with key <paramref name="key"/></para>
+		/// <para>Or add a new <typeparamref name="TValue"/> to the dictionary with that key and return it</para>
+		/// </summary>
+		/// <param name="dictionary">The dictionary to get the value from</param>
+		/// <param name="key">The key of the KeyValuePair to get the value from</param>
+		/// <typeparam name="TKey">The type of the Key in the dictionary</typeparam>
+		/// <typeparam name="TValue">The type of the Value in the dictionary</typeparam>
+		/// <returns>The value of the KeyValuePair at <paramref name="key"/> or a new <typeparamref name="TValue"/></returns>
+		public static TValue GetOrAddNewValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key) where TValue : new()
+		{
+			if (!dictionary.TryGetValue(key, out TValue value))
+			{
+				value = new TValue();
+				dictionary.Add(key, value);
+			}
+			
+			return value;
+		}
+
+		/// <summary>
+		/// <para>Get the value of the KeyValuePair with key <paramref name="key"/></para>
+		/// <para>Or add a new <typeparamref name="TValue"/> to the dictionary with that key and return it</para>
+		/// </summary>
+		/// <param name="dictionary">The dictionary to get the value from</param>
+		/// <param name="key">The key of the KeyValuePair to get the value from</param>
+		/// <param name="factoryFunction">A function to create a new <typeparamref name="TValue"/> (only invoked if <paramref name="key"/> is not present in the dictionary)</param>
+		/// <typeparam name="TKey">The type of the Key in the dictionary</typeparam>
+		/// <typeparam name="TValue">The type of the Value in the dictionary</typeparam>
+		/// <returns>The value of the KeyValuePair at <paramref name="key"/> or a new <typeparamref name="TValue"/></returns>
+		public static TValue GetOrAddNewValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Func<TValue> factoryFunction)
+		{
+			if (!dictionary.TryGetValue(key, out TValue value))
+			{
+				value = factoryFunction.Invoke();
+				dictionary.Add(key, value);
+			}
+			
+			return value;
+		}
+	}
+}
